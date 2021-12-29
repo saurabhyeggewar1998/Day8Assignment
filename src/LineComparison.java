fc553446bcacedb461378d318932628e61930f60
saurabhyeggewar1998 <95201793+saurabhyeggewar1998@users.noreply.github.com>
2021-12-29 11:34:18 +0530

    Update LineComparison.java

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
-        GetInput input = new GetInput();
-        input.getInput();
-        CalculateLength cal = new CalculateLength();
-        cal.calLength();
+        LengthProduce input = new LengthProduce();
+        input.lengthproduce();
+        LengthCalculate cal = new LengthCalculate();
+        cal.Lengthcal();
         CompareLength compare = new CompareLength();
         compare.checkEquality();
     }
 }
-class GetInput{
+class LengthProduce{
     public static int x1,x2,y1,y2,x3,x4,y3,y4;
-    public  void getInput() {
+    public  void lengthproduce() {
         Scanner sc = new Scanner(System.in);
         System.out.println("For given line");
         System.out.println("Enter co-ordinates of x1 and y1");
@@ -30,21 +30,21 @@ class GetInput{
         y4 = sc.nextInt();
     }
 }
-class CalculateLength{
+class LengthCalculate{
     static Double length1,length2;
-    public void calLength() {
-        length1 = Math.sqrt((GetInput.x2 - GetInput.x1) * (GetInput.x2 - GetInput.x1) + (GetInput.y2 - GetInput.y1) * (GetInput.y2 - GetInput.y1));
+    public void Lengthcal() {
+        length1 = Math.sqrt((LengthProduce.x2 - LengthProduce.x1) * (LengthProduce.x2 - LengthProduce.x1) + (LengthProduce.y2 - LengthProduce.y1) * (LengthProduce.y2 - LengthProduce.y1));
         System.out.println("Length of first line = "+length1+" units");
-        length2 = Math.sqrt((GetInput.x4 - GetInput.x3) * (GetInput.x4 - GetInput.x3) + (GetInput.y4 - GetInput.y3) * (GetInput.y4 - GetInput.y3));
+        length2 = Math.sqrt((LengthProduce.x4 - LengthProduce.x3) * (LengthProduce.x4 - LengthProduce.x3) + (LengthProduce.y4 - LengthProduce.y3) * (LengthProduce.y4 - LengthProduce.y3));
         System.out.println("Length of second line = "+length2+" units");
     }
 }
 class CompareLength{
     public  void checkEquality() {
-        if(CalculateLength.length1.compareTo(CalculateLength.length2) == 1) {
+        if(LengthCalculate.length1.compareTo(LengthCalculate.length2) == 1) {
             System.out.println("Line1 is greater than line2 in length");
         }
-        else if (CalculateLength.length1.compareTo(CalculateLength.length2) == -1) {
+        else if (LengthCalculate.length1.compareTo(LengthCalculate.length2) == -1) {
             System.out.println("Line1 is less than line2 in length");
         }
         else {
@@ -52,3 +52,4 @@ class CompareLength{
         }
     }
 }
+    
